b12cab3e1f9db72fe30d24b8db26b4ce987ea53a
TheManInTheMaze <c.pescheloche@gmail.com>
2015-07-07 21:31:54 +0200

    JUNIT - Square testing

---
+++ src/test/java/model/SquareTest.java
@@ -0,0 +1,15 @@
+package model;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Created by pcorentin on 07/07/2015.
+ */
+public class SquareTest extends TestCase {
+
+    @Test
+    public void testClic() throws Exception {
+
+    }
+}

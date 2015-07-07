7767a6ea490e5bbe7f19b0123e7e428425caa0c1
TheManInTheMaze <c.pescheloche@gmail.com>
2015-07-07 22:29:19 +0200

    JUNIT - Grid first clic test

--- src/test/java/model/GridTest.java
+++ src/test/java/model/GridTest.java
@@ -1,9 +1,9 @@
 package model;
 
+import model.interfaces.IGrid;
 import org.junit.Test;
 
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertSame;
+import static org.junit.Assert.*;
 
 /**
  * Created by pcorentin on 07/07/2015.
@@ -13,15 +13,6 @@ public class GridTest {
     private final int MAXCOLUMNSIZE = 10;
 
 
-    @Test
-    public void testGetStatus() throws Exception {
-
-    }
-
-    @Test
-    public void testSetStatus() throws Exception {
-
-    }
 
     @Test
     public void testGetGrid() throws Exception {
@@ -32,25 +23,17 @@ public class GridTest {
             for (int columnSize = 1; columnSize < MAXCOLUMNSIZE; columnSize++) {
                 for (int nbMines = 0; nbMines < rowSize * columnSize; nbMines++) {
                     Grid grid = new Grid(rowSize, columnSize, nbMines);
-                    // to trigger mine positions
+                    // to trigger mine positioning
                     grid.clic(0, 0);
                     Square[][] actualGrid = (Square[][]) grid.getGrid();
-                    int cntMines = 0;
-                    for (Square[] rows : actualGrid) {
-                        for (Square square : rows) {
-                            if (square.isHasMine()) {
-                                cntMines++;
-                            }
-                        }
-                    }
-                    assertSame(nbMines, cntMines);
+                    assertSame(nbMines, getCntMines(actualGrid));
                 }
             }
         }
 
         // TEST WRONG INPUTS
         assertNull(new Grid(-1, 3, 2));
-        assertNull(new Grid(4, 1, 2));
+        assertNull(new Grid(4, -1, 2));
         assertNull(new Grid(2, 3, -1));
         assertNull(new Grid(0, 1, 0));
         assertNull(new Grid(1, 0, 0));
@@ -60,26 +43,51 @@ public class GridTest {
         assertNull(new Grid(1, 1, 1));
         assertNull(new Grid(1, 1, 2));
 
+    }
+
+    private int getCntMines(Square[][] actualGrid) {
+        int cntMines = 0;
+        for (Square[] rows : actualGrid) {
+            for (Square square : rows) {
+                if (square.isHasMine()) {
+                    cntMines++;
+                }
+            }
+        }
+        return cntMines;
+    }
+
+
+    @Test
+    public void testFirstClic() throws Exception {
+        Grid grid = new Grid(3, 3, 2);
+        assertSame(IGrid.GameStatus.STARTED, grid.getStatus());
+
+        Square[][] actualGrid = (Square[][]) grid.getGrid();
+        int previousCntMines = getCntMines(actualGrid);
+        grid.clic(1, 1);
+        assertNotEquals(previousCntMines, actualGrid);
+        // WE SHALL NEVER LOOSE ON FIRST CLIC
+        assertSame(IGrid.GameStatus.ONGOING, grid.getStatus());
+
 
     }
 
     @Test
-    public void testSetGrid() throws Exception {
+    public void testWinningGame() throws Exception {
+
+    }
+
+
+    @Test
+    public void testLosingGame() throws Exception {
 
     }
 
     @Test
-    public void testGetNbMines() throws Exception {
+    public void testSquareNumbers() {
 
     }
 
-    @Test
-    public void testSetNbMines() throws Exception {
 
-    }
-
-    @Test
-    public void testClic() throws Exception {
-
-    }
 }

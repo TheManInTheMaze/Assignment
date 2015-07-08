9ef238de7198dd5cc86e8645513b0ca158e128ad
TheManInTheMaze <c.pescheloche@gmail.com>
2015-07-08 23:00:32 +0200

    creation of class Main application

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,13 @@
+package application;
+
+/**
+ * Created by pcorentin on 08/07/2015.
+ */
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath("api")
+public class Main extends Application {
+}

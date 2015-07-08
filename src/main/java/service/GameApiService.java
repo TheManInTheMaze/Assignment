5a4921a2b35946375f42efaa5e147d8487af7172
TheManInTheMaze <c.pescheloche@gmail.com>
2015-07-08 23:40:35 +0200

    IMPLEMENTATION - Set path to minesweep api

--- src/main/java/service/GameApiService.java
+++ src/main/java/service/GameApiService.java
@@ -3,14 +3,12 @@ package service;
 import model.interfaces.IGrid;
 
 import javax.inject.Inject;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.GET;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 
 /**
  * Created by pcorentin on 08/07/2015.
  */
+@Path("/minesweep")
 public class GameApiService {
 
     @Inject
